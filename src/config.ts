8984546148dca74ea6346e918313e7ecf7ba9061
LAXMICHANDRA-MANNE <manne.laxmichandra@gmail.com>
2026-04-25 01:30:42 +0530

    Add objfile.obj and update config (excluding files > 100MB)

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const isProd = import.meta.env.PROD;
 
 export const API_URLS = {
-  NODE_BACKEND: isProd ? 'https://xrstarter-node-backend.onrender.com' : 'http://localhost:3000',
+  NODE_BACKEND: isProd ? 'https://xrstarter-node-backend.onrender.com' : 'http://localhost:4000',
   PYTHON_ENGINE: isProd ? 'https://xrstarter-python2-engine.onrender.com' : 'http://localhost:5000',
 };
